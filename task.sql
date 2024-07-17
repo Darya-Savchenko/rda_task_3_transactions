82f817fdd6b197200d68463cd8d251412f92181e
Darya-Savchenko <savchenkodarya277@gmail.com>
2024-07-17 15:00:43 +0300

    correct syntax mistake

--- task.sql
+++ task.sql
@@ -8,6 +8,6 @@ START TRANSACTION;
 INSERT INTO OrderItems (OrderID, ProductID, Count)
 	VALUES ('1', '1', 1);
 
-UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = '1'
+UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = '1';
 
 COMMIT;
